4b10bd2f308e91f140e4feb32d8f6ad62634b538
Sa18-f <sisazid182713@gmail.com>
2024-02-17 18:58:04 +0600

    new section added

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,17 @@ const allBtn = document.getElementsByClassName('add-btn');
 for(const btn of allBtn){
     btn.addEventListener('click', function(e){
         count = count + 1;
+        const placeName = e.target.parentNode.childNodes[1].innerText;
+        const price = e.target.parentNode.childNodes[3].childNodes[1].innerText;
+        const selectContainer = document.getElementById('selected-place-container');
+        const li = document.createElement('li');
+        const p = document.createElement('p');
+        p.innerText = placeName;
+        const p2 = document.createElement('p');
+        p2.innerText = price;
+        li.appendChild(p);
+        li.appendChild(p2);
+        selectContainer.appendChild(li);
         setInnerText('cart-count', count);
     })
 }
